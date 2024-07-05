29b116ed11f7c2f2e35f6e1acaeab477480932ca
rcb0263 <149785947+rcb0263@users.noreply.github.com>
2024-07-05 12:37:57 +0200

    Add files via upload

--- Extraordinario/main_test.ts
+++ Extraordinario/main_test.ts
@@ -1,6 +1,13 @@
-import { assertEquals } from "jsr:@std/assert";
-import { add } from "./main.ts";
+
 
 Deno.test(function addTest() {
   assertEquals(add(2, 3), 5);
 });
+
+function assertEquals(arg0: any, arg1: number) {
+  throw new Error("Function not implemented.");
+}
+
+function add(arg0: number, arg1: number): any {
+  throw new Error("Function not implemented.");
+}
